8a4c4ee3c69de91faf19c0a90581da53a6d283f2
guilherme magalhães <123709550+bymagalhaes@users.noreply.github.com>
2024-02-20 15:36:36 -0300

    ADD INTERNAL PULLUP RESISTOR

--- key_password V2.0/V2-key_password.ino
+++ key_password V2.0/V2-key_password.ino
@@ -1,9 +1,9 @@
 #include "DigiKeyboard.h"
 
 void setup() {
-    pinMode(0, INPUT);
-    pinMode(3, INPUT);
-    pinMode(2, INPUT);
+    pinMode(0, INPUT_PULLUP);
+    pinMode(3, INPUT_PULLUP);
+    pinMode(2, INPUT_PULLUP);
     
     for (int i = 0; i != 10; i++) { //para que não acenda o led comente esse for
         delay(200); //dita o tempo de traso em milissegundos
@@ -14,19 +14,18 @@ void setup() {
 }
 
 void loop() {
-    if (digitalRead(0) == 1 && digitalRead(0) == 0) {
+    if (digitalRead(0) == 0 && digitalRead(0) == 1) {
         DigiKeyboard.print("senha 1");
         DigiKeyboard.delay(200);
     }
 
-    if (digitalRead(3) == 1 && digitalRead(1) == 0) {
+    if (digitalRead(3) == 0 && digitalRead(3) == 1) {
         DigiKeyboard.print("senha 2");
         DigiKeyboard.delay(200);
     }
 
-    if (digitalRead(2) == 1 && digitalRead(2) == 0) {
+    if (digitalRead(2) == 0 && digitalRead(2) == 1) {
         DigiKeyboard.print("senha 3");
         DigiKeyboard.delay(200);
     }
 }
-//esse codigo foi criado por guilherme magalhães.
